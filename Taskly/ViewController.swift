298e13a35b2d3f563e0ebbd52e7addafe1775688
cagatay <cagatay.gedik@icloud.com>
2022-11-02 23:50:41 +0300

    Initial Commit

---
+++ Taskly/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Taskly
+//
+//  Created by Celil Çağatay Gedik on 2.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
