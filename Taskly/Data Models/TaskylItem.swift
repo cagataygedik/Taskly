a692184c38b041c2792926496a3d190a8726c0de
cagatay <cagatay.gedik@icloud.com>
2022-11-04 00:00:41 +0300

    added TasklyItem class.

---
+++ Taskly/Data Models/TaskylItem.swift
@@ -0,0 +1,13 @@
+//
+//  TaskylItem.swift
+//  Taskly
+//
+//  Created by Celil Çağatay Gedik on 3.11.2022.
+//
+
+import Foundation
+
+class TasklyItem {
+    var text = ""
+    var checked = false
+}
