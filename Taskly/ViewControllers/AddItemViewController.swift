f3c108668119e65e4760852f58fcf068c7763743
cagatay <cagatay.gedik@icloud.com>
2022-11-06 14:18:18 +0300

    Added TextField section

--- Taskly/ViewControllers/AddItemViewController.swift
+++ Taskly/ViewControllers/AddItemViewController.swift
@@ -7,11 +7,20 @@
 
 import UIKit
 
-class AddItemViewController: UITableViewController {
+class AddItemViewController: UITableViewController, UITextFieldDelegate {
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var doneBarButton: UIBarButtonItem!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
     
+    }
+    
+    // Keyboard automatically shows up when we navigate to the screen.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        textField.becomeFirstResponder()
     }
     
     // MARK: - Actions
@@ -21,7 +30,46 @@ class AddItemViewController: UITableViewController {
     }
     
     @IBAction func done() {
+        print("Contents of text field: \(textField.text!)")
+        
         navigationController?.popViewController(animated: true)
     }
+    
+    // MARK: - Table View Delegates
+    
+    // Disabled cell selection in text field.
+    override func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+        return nil
+    }
+    
+    // MARK: - Text Field Delegates
+    
+    // It gives us which part of the text should replaced with our new-writed text.
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        // We should calculate what the new text will be by taking the text field's text and doing the replacement yourself.
+        let oldText = textField.text!
+        let stringRange = Range(range, in: oldText)!
+        
+        // It gives us a new string object that we store in the newText constant.
+        let newText = oldText.replacingCharacters(in: stringRange, with: string)
+        
+        //If text field is empty disable the done button
+        if newText.isEmpty {
+            doneBarButton.isEnabled = false
+            
+        //If text field is filled enable the done button
+        } else {
+            doneBarButton.isEnabled = true
+        }
+        return true
+    }
+    
+    // When we pressed the clear button in text field, the done button was not disabled. This code is a solution for it.
+    func textFieldShouldClear(_ textField: UITextField) -> Bool {
+        doneBarButton.isEnabled = false
+        return true
+    }
+    
     
 }
