330d554f092da480101873264331e8066bae9c23
cagatay <cagatay.gedik@icloud.com>
2022-11-11 00:25:24 +0300

    New data model for upcoming ViewControllers(All List & List Detail).

---
+++ Taskly/Data Models/Taskly.swift
@@ -0,0 +1,19 @@
+//
+//  Taskly.swift
+//  Taskly
+//
+//  Created by Celil Çağatay Gedik on 10.11.2022.
+//
+
+import UIKit
+
+class Taskly: NSObject {
+
+    var name = ""
+    
+    init(name: String) {
+        self.name = name
+        super.init()
+    }
+    
+}
